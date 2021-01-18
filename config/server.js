8a79a3d618a3451ae8d6570a4438862916e3647a
streaper2 <streaper2@gmail.com>
2021-01-18 06:45:39 +0100

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,10 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    url:'/dashboard',
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '7d8b98b835d643c23b3587eac6cb7a9a'),
-    },
-  },
+  url: env('HEROKU_URL'),
 });
